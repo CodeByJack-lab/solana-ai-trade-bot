8027ed3671a5e3d86459f2dd7e944658fd360ce8
Jack <cyanjim@gmail.com>
2026-03-20 18:36:09 +0800

    fix(v5.5-final： updated tradeService

--- src/services/tradeService.js
+++ src/services/tradeService.js
@@ -324,4 +324,4 @@ async function commitTradeToDb(posIndex, sellValueSol, finalPriceSol, pnlSol, pn
     }
 }
 
-module.exports = { executeBuy, executeSell, executeSellRaydium, forceWriteOff };
+module.exports = { executeBuy, executeSell, executeSellRaydium, forceWriteOff, runSellPipeline };
